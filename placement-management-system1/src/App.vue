70a07da17d0e8ef66aefe7487182a15f825389d9
projectvinay <projectvinay5@gmail.com>
2026-06-24 23:10:58 +0530

    add my full project

---
+++ placement-management-system1/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from './components/navbar.vue';
+import leftsidesection from './components/leftsidesection.vue';
+</script>
+
+<template>
+  <Navbar />
+  <leftsidesection/>
+</template>
---
+++ placement-management-system1/src/components/leftsidesection.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="h-screen flex items-center justify-center w-[10vw] bg-white">
+        <ul class="space-y-2 ">
+            <li v-for="(item, index) in leftsideItems" :key="index"
+            :ref="el => menuItems[index] = el"
+                class="menu-item w-40 gap-3 p-3 rounded-xl cursor-pointer flex items-center" @mouseenter="hoverIn(index)"
+                @mouseleave="hoverout(index)">
+                <img :src="item.logo" :alt="item.title" class="h-7 w-7" />
+                <p>{{ item.title }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import { gsap } from "gsap";
+import dashboardImg from "../svg/dashboard-fill.svg";
+import JobListing from "../svg/briefcase-2-line.svg";
+
+const menuItems = ref([]);
+
+const leftsideItems = [
+    { logo: dashboardImg, title: "Dashboard" },
+      { logo: JobListing, title: "Job Listing" },
+    //   { logo: applicationIcon, title: "My Application" },
+    //   { logo: profileIcon, title: "Profile" },
+    //   { logo: logoutIcon, title: "Logout" },
+];
+
+const hoverIn = (index) => {
+    gsap.to(menuItems.value[index], {
+        backgroundColor: "#2563EB",
+        color: "#ffffff",
+        scale: 1.05,
+        duration: 0.3,
+        ease: "power2.out"
+    });
+};
+const hoverout = (index) => {
+        gsap.to(menuItems.value[index], {
+            backgroundColor: "#ffffff",
+            color: "#000000",
+            scale:1,
+            duration:0.3,
+            ease:"power2.out",
+        })
+    };
+</script>
